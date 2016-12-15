625242a19b2ace5c55d35a6ee6bde2aab727d0cd
Jesús Campón <jesus.campon@gmail.com>
2016-12-15 02:57:45 +0000

    Adding initial application scaffolding

---
+++ RockPaperScissors.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using NUnit.Framework;
+using RockPaperScissors;
+using RockPaperScissors.Web.Controllers;
+
+namespace RockPaperScissors.Tests.Controllers
+{
+    [TestFixture]
+    public class HomeControllerTest
+    {
+        [Test]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
